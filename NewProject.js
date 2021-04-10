0092f86c0a216ce144601777988a514a7bb4abe9
Anthony Turinski <nodeathking@gmail.com>
2021-04-10 14:49:01 -0700

    more

---
+++ NewProject.js
@@ -0,0 +1,18 @@
+/*
+
+create empty repo online
+npx create-react-app 301n21-lab00
+
+cd 301n21-lab00
+git remote add origin https://github.com/Terransky/301n21-lab00.git
+git branch -M main
+git push -u origin main
+
+remove 4 files: setupTests.js, reportWebVitals.js, logo.svg, App.test.js
+edit index.js: delete reportWebVitals related lines, add: import 'bootstrap/dist/css/bootstrap.min.css;
+edit App.js: remove function body, delete import logo, add import React from 'react'; , class App extends React.Component {}
+
+npm install axios
+npm install bootstrap react-bootstrap
+
+*/
